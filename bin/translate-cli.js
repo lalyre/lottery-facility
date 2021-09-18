3343a34cafe3e2f48c91ceaa29496e61b96bc61b
lalyre <c_lalyre@yahoo.fr>
2021-09-18 12:06:16 +0200

    cl

--- bin/translate-cli.js
+++ bin/translate-cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 'use strict'
+const fs = require('fs');
 const meow = require('meow');
 const lotteryFacility = require('../dist/lotteryfacility-nodebundle');
 
@@ -20,53 +21,116 @@ const cli = meow(`
 	The optional parameter 'sort' sorts combinations items in ascending order.
 `, {
 	flags: {
-		total: {
-			type: 'number',
-			alias: 't',
-			isRequired: (input, flags) => true,
+		originfile: {
+			type: 'string',
+			isRequired: (input, flags) => {
+				if (flags.originnum) {
+					return false;
+				}
+				return true;
+			},
+			isMultiple: false,
 		},
-		size: {
-			type: 'number',
-			alias: 's',
-			isRequired: (input, flags) => true,
+		originnum: {
+			type: 'string',
+			isRequired: false,
+			isMultiple: false,
 		},
-		sort: {
-			type: 'boolean',
-			default: false,
+		targetfile: {
+			type: 'string',
+			isRequired: (input, flags) => {
+				if (flags.targetnum) {
+					return false;
+				}
+				return true;
+			},
+			isMultiple: false,
 		},
-		nb: {
-			type: 'number',
-			default: 1,
+		targetnum: {
+			type: 'string',
+			isRequired: false,
+			isMultiple: false,
 		},
-		nbSwap: {
-			type: 'number',
-			default: 35,
+		file: {
+			type: 'string',
+			alias: 'f',
+			isRequired: true,
+			isMultiple: false,
 		},
 	}
 });
 
 
-/*if (cli.flags.total < 1 || cli.flags.total > 99) {
-	console.error("wrong value for <total> parameter !");
-	process.exit(1);
-}
-if (cli.flags.total < cli.flags.size) {
-	console.error("wrong value for <size> parameter !");
+if (!fs.existsSync(cli.flags.file)) {
+	console.error(`File ${cli.flags.file} does not exist`);
 	process.exit(1);
 }
 
 
-let total = cli.flags.total;
-let size = cli.flags.size;
-let nb = cli.flags.nb;
-let nbSwap = cli.flags.nbSwap;
-let balls = lotteryFacility.lotteryBalls(total);
-let cb = null;
-let str = null;
+let origin_alphabet = null;
+if (cli.flags.originnum) {
+	origin_alphabet = cli.flags.originnum.trim().split(/\|/);
+} else {
+	if (!fs.existsSync(cli.flags.originfile)) {
+		console.error(`File ${cli.flags.originfile} does not exist`);
+		process.exit(1);
+	}
+	origin_alphabet = fs.readFileSync(cli.flags.originfile).toString().trim().split(/\r?\n/);
+}
 
-for (let i = 0; i < nb; i++) {
-	lotteryFacility.shuffleBalls(balls, nbSwap);
-	cb = balls.slice(1, size+1);
-	str = (cli.flags.sort) ? lotteryFacility.canonicalCombinationString(cb, " ") : lotteryFacility.combinationString(cb, " ");
-	console.log(str);
-}*/
+
+let target_alphabet = null;
+if (cli.flags.targetnum) {
+	target_alphabet = cli.flags.targetnum.trim().split(/\|/);
+} else {
+	if (!fs.existsSync(cli.flags.targetfile)) {
+		console.error(`File ${cli.flags.targetfile} does not exist`);
+		process.exit(1);
+	}
+	target_alphabet = fs.readFileSync(cli.flags.targetfile).toString().trim().split(/\r?\n/);
+}
+
+
+if (origin_alphabet.length > target_alphabet.length) {
+	console.error("The target alphabet does not contain enough items");
+	process.exit(1);
+}
+
+
+
+/*
+
+var fileStream = fs.createReadStream(filename);
+var rl = readline.createInterface({
+	input: fileStream,
+	crlfDelay: Infinity
+});
+
+rl.on('line', (line) => {
+	if (!line) {
+		return;
+	}
+	var res1 = line.trim().split(/\s+/);
+	//console.log("res1 " + res1);
+	
+	var res2 = res1.map(o => {
+		var id = from_numbers.findIndex(obj => obj == o);
+		if (id == -1) {
+			return `>>>${o}<<<`;
+		}
+		return to_numbers[id];
+	});
+	//console.log("res2 " + res2);
+	
+	var result_line = res2.join(" ");
+	console.log(result_line);
+})
+.on('close', () => {
+	//console.log('Have a great day!');
+	//process.exit(0);
+});
+
+
+*/
+
+
