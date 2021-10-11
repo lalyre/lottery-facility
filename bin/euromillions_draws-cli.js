1351824b497502576e994395b60b2048b73d282b
Claude Lalyre <clalyre@ektacom.com>
2021-10-11 11:31:54 +0200

    cl

--- bin/euromillions_draws-cli.js
+++ bin/euromillions_draws-cli.js
@@ -9,161 +9,49 @@ const lotteryFacility = require('../dist/lotteryfacility-nodebundle');
 
 const cli = meow(`
 	Usage
-	  $ translate
+	  $ euromillions_draws
 
 	Parameters
-	  --file, -f    A file containing one combination per line
-	  --originnum   Items of combinations separated by '|' that are used in combinations of <file> file
-	  --originfile  File containing one item per line that are used in combinations of <file> file
-	  --targetnum   Items of combinations used for the translation of <file> file
-	  --targetfile  File containing one item per line used for the translation <file> file
+	  --num, -n     It retrieves the last <num> draws of Euromillions lottery in ascending order
+	  --date		Displays the date
+	  --stars		Displays the stars
+	  --head		Gives only the first <head> lines instead of the whole required draws in ascending order
 	  
 	Description
-	This script takes an input file <file> containing one combination per line, combinations
-	written with the <origin> alphabet and translated into <target> alphabet.
-	Items of <origin> alphabet are translated to <target> alphabet relatively to
-	their corresponding order of declaration.
-
-	The <origin> alphabet can be declared either with <originnum> or <originfile> parameters.
-	The <target> alphabet can be declared either with <targetnum> or <targetfile> parameters.
+	This script gives last draws of Euromillions lottery.
 `, {
 	flags: {
-		originfile: {
+		num: {
 			type: 'string',
-			isRequired: (input, flags) => {
-				if (input.originnum) {
-					return false;
-				}
-				return true;
-			},
-			isMultiple: false,
-		},
-		originnum: {
-			type: 'string',
-			isRequired: false,
-			isMultiple: false,
-		},
-		targetfile: {
-			type: 'string',
-			isRequired: (input, flags) => {
-				if (input.targetnum) {
-					return false;
-				}
-				return true;
-			},
-			isMultiple: false,
-		},
-		targetnum: {
-			type: 'string',
-			isRequired: false,
-			isMultiple: false,
-		},
-		file: {
-			type: 'string',
-			alias: 'f',
+			alias: 'n',
 			isRequired: true,
 			isMultiple: false,
 		},
+		date: {
+			type: 'boolean',
+			default: false,
+			isRequired: false,
+			isMultiple: false,
+		},
+		stars: {
+			type: 'boolean',
+			default: false,
+			isRequired: false,
+			isMultiple: false,
+		},
+		head: {
+			type: 'number',
+			isRequired: false,
+			isMultiple: false,
+		},
 	}
 });
 
 
-if (!fs.existsSync(cli.flags.file)) {
-	console.error(`File ${cli.flags.file} does not exist`);
-	process.exit(1);
-}
-
-
-let origin_alphabet = null;
-if (cli.flags.originnum) {
-	origin_alphabet = cli.flags.originnum.trim().split(/\|/);
-} else {
-	if (!fs.existsSync(cli.flags.originfile)) {
-		console.error(`File ${cli.flags.originfile} does not exist`);
-		process.exit(1);
-	}
-	origin_alphabet = fs.readFileSync(cli.flags.originfile).toString().trim().split(/\r?\n/);
-}
-
-
-let target_alphabet = null;
-if (cli.flags.targetnum) {
-	target_alphabet = cli.flags.targetnum.trim().split(/\|/);
-} else {
-	if (!fs.existsSync(cli.flags.targetfile)) {
-		console.error(`File ${cli.flags.targetfile} does not exist`);
-		process.exit(1);
-	}
-	target_alphabet = fs.readFileSync(cli.flags.targetfile).toString().trim().split(/\r?\n/);
-}
-
-
-if (origin_alphabet.length > target_alphabet.length) {
-	console.error("The target alphabet does not contain enough items");
-	process.exit(1);
-}
-
-
-
-
-
-
-
-
-
-
-if (process.argv.length <= 2
-|| (process.argv[2] == '-h' || process.argv[2] == '--help')) {
-	console.log("euromillions_lastdraws.js [Version " + version.api_version + "]")
-	console.log("(c) 2020 Claude Lalyre Corporation. Tous droits reserves.")
-	console.log("Usage:");
-	console.log("euromillions_lastdraws.js --help");
-	console.log("euromillions_lastdraws.js --version");
-	console.log("euromillions_lastdraws.js -n <number of last draws> [-head <number of lines>] [-date] [-stars]");
-	console.log("\t");
-	console.log("");
-	console.log("This script gives last draws of Euromillions lottery.");
-	console.log("The -n <num> parameter gives the last <num> draws of Euromillions lottery.")
-	console.log("The optional [-date] parameter also displays date.");
-	console.log("The optional [-stars] parameter also displays the stars.");
-	console.log("The optional [-head <num>] gives only the first <num> draws.")
-	process.exit(0);
-}
-if (process.argv[2] == '-v' || process.argv[2] == '-V' || process.argv[2] == '--version') {
-	console.log(`v${version.api_version}`);
-	process.exit(0);
-}
-
-
-var num = undefined;
-var head = undefined;
-var display_date = false;
-var display_stars = false;
-process.argv.forEach((val, index) => {
-	if (val == '-n') {
-		if (index + 1 == process.argv.length) {
-			console.error("Missing argument on command line");
-			process.exit(1);
-		}
-		val = process.argv[index+1].trim();
-		num = parseInt(val, 10);
-	}
-	if (val == '-date') {
-		display_date = true;
-	}
-	if (val == '-stars') {
-		display_stars = true;
-	}
-	if (val == '-head') {
-		if (index + 1 == process.argv.length) {
-			console.error("Missing argument on command line");
-			process.exit(1);
-		}
-		val = process.argv[index+1].trim();
-		head = parseInt(val, 10);
-	}
-});
-
+let num = cli.flags.num;
+let head = cli.flags.head;
+let displayDate = cli.flags.date;
+let displayStars = cli.flags.stars;
 
 /*
  * https://www.fdj.fr/jeux-de-tirage/euromillions-my-million/statistiques
@@ -228,10 +116,10 @@ rp({
 		var stars = values.slice(10, 12);		
 
 		var display = balls.map (x => x.toString().padStart(2, 0)).join(" ")
-		if (display_stars) {
+		if (displayStars) {
 			display += " / " + stars.map (x => x.toString().padStart(2, 0)).join(" ");
 		}
-		if (display_date) {
+		if (displayDate) {
 			display = date + "; " + display;
 		}
 		console.log(display);
