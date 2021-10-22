1535b23a5b7f8c744475397111dfe0033a7f9569
Claude Lalyre <clalyre@ektacom.com>
2021-10-22 17:50:20 +0200

    cl

--- bin/combination-cli.js
+++ bin/combination-cli.js
@@ -14,12 +14,12 @@ const cli = meow(`
 	  --total, -t     Total number of lottery balls
 	  --size, -s      Size of generated combinations
 	  --file, -f      A file containing one item of combination per line
-	  --numbers, -n   Items of combinations separated by '|', or ' '.
+	  --numbers, -n   Items of combinations separated by '|', or ' '
 
 	Description
 	This script generates combinations of items taken in <file> or <numbers>, of size <size>
 	implementing choice of <size> items among <total> items.
-    Only the first <total> items of <file> or <numbers> are used to build combinations.
+	Only the first <total> items of <file> or <numbers> are used to build combinations.
 `, {
 	flags: {
 		total: {
