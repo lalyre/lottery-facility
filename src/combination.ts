32de3781974794a5504a285cfa538a95e03558a9
lalyre <c_lalyre@yahoo.fr>
2022-05-18 00:28:46 +0200

    cl

--- src/combination.ts
+++ src/combination.ts
@@ -186,6 +186,7 @@ export class Combination {
 	public static binomial(max:number, n:number): number {
 		if (max <= 0) return 0;
 		if (n > max) return 0;
+		if (n === 0) return 1;
 		if (n === max) return 1;
 		let ret:number = Combination.factorial(max);
 		ret /= Combination.factorial(n);
