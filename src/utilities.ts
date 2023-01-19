99260672e932318654d2f31d2afebe46cd0e030b
Claude Lalyre <clalyre@ektacom.com>
2023-01-19 12:21:38 +0100

    CL

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,9 +2,9 @@
 
 
 /**
- * Get GMT date YYYYMMDDHHMMSS from Javascript date
+ * Get GMT date YYYY-MM-DD HH:MM:SS.NNNZ from Javascript date
  * @param date			a Javascript date object
- * @return				a string representing the date in format YYYYMMDDHHMMSS
+ * @return				a string representing the date in format 'YYYY-MM-DD HH:MM:SS.NNNZ'
  */
 export function displayUTCDateTime(date: Date): string|null {
 	if (!date) {
@@ -16,6 +16,28 @@ export function displayUTCDateTime(date: Date): string|null {
 	const hour = date.getUTCHours().toString().padStart(2, '0');
 	const minute = date.getUTCMinutes().toString().padStart(2, '0');
 	const second = date.getUTCSeconds().toString().padStart(2, '0');
-	const display = `${year}${month}${day}${hour}${minute}${second}`;
+	const millis = date.getUTCMilliseconds().toString().padStart(3, '0');
+	const display = `${year}-${month}-${day} ${hour}:${minute}:${second}.${millis}Z`;
+	return display;
+}
+
+
+/**
+ * Get GMT date YYYYMMDDHHMMSSNNN from Javascript date
+ * @param date			a Javascript date object
+ * @return				a string representing the date in format 'YYYYMMDDHHMMSSNNN'
+ */
+export function displayUTCDateTimeYYYYMMDDHHMMSSNNN(date: Date): string|null {
+	if (!date) {
+		return null;
+	}
+	const year = date.getUTCFullYear();
+	const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+	const day = date.getUTCDate().toString().padStart(2, '0');
+	const hour = date.getUTCHours().toString().padStart(2, '0');
+	const minute = date.getUTCMinutes().toString().padStart(2, '0');
+	const second = date.getUTCSeconds().toString().padStart(2, '0');
+	const millis = date.getUTCMilliseconds().toString().padStart(3, '0');
+	const display = `${year}${month}${day}${hour}${minute}${second}${millis}`;
 	return display;
 }
