921ba974bb34a8ce3c83ccc4158a4c460944f462
Claude Lalyre <clalyre@ektacom.com>
2025-12-11 15:47:02 +0100

    Fix

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+'use strict';
+export * from './version';
+export * from './utilities';
+export * from './random';
+export * from './drawbox';
+export * from './tuple';
+export * from './cartesian_product';
+
