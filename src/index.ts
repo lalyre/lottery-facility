85409e1b72e3d0e4847aef3427bb29c3dd1cd2de
lalyre <c_lalyre@yahoo.fr>
2025-12-12 22:33:27 +0100

    cl

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-export * from './version';
-export * from './utilities';
-export * from './random';
-export * from './drawbox';
-export * from './tuple';
-export * from './cartesian_product';
-export * from './combination';
-
